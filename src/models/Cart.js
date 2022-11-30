8c976086f7a8b87e82128429e96100c247d9affb
pccomponentshenry <pccomponentshenry@gmail.com>
2022-11-29 23:08:58 -0300

    create model filters

---
+++ src/models/Cart.js
@@ -0,0 +1,13 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('cart', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUID,
+        primaryKey : true
+      },
+  });
+};

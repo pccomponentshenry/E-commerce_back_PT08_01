6badecffd29bb52afc5e29f8d873eb6c382e19bd
carlamadeo <carlamadeo@gmail.com>
2022-11-29 23:57:36 -0300

    Users Models

---
+++ src/models/Users.js
@@ -0,0 +1,53 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('user', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUID,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
+      validate: {
+        notNull: {
+          msg: `Username can't be empty`
+        },
+        len: {
+          args: [3, 20],
+          msg: 'Username must have 3 to 20 characters'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(100),
+      allowNull: false,
+      unique: {
+        args: true,
+        msg: 'Email address already in use'
+      },
+      validate: {
+        isEmail: {
+          msg: 'Please provide a valid email'
+        },
+      }
+    },
+    category: {
+      type: DataTypes.ENUM('buyer', 'seller')
+    },
+    status: {
+      type: DataTypes.ENUM('active', 'inactive', 'banned', 'deleted')
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    }
+  })
+};

ca3589fa431c9275792f48a9204226413b11b2b3
Diego Cubillos <dcubillos12@hotmail.com>
2022-11-30 19:27:56 -0500

    feat(routes/index.js): add rutes generics
    (brand - cart - cartItem)

---
+++ src/routes/brand.js
@@ -0,0 +1,6 @@
+//const axios = require("axios");
+const { Router } = require("express");
+const router = Router();
+
+
+module.exports = router;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
+const brand = require("./brand.js")
+const cart = require("./cart.js")
+const cartItem = require("./cartItem.js")
 
-const router = Router();
+const router = Router()
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 //probando el pullRequest  
+router.use("/brand", brand)
+router.use("/cart", cart)
+router.use("/cartItem", cartItem)
 
 
 module.exports = router;

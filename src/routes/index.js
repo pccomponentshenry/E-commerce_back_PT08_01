e4a2ad2adccbc49aa51420f03655ee57c98a4902
Diego Cubillos <dcubillos12@hotmail.com>
2022-11-30 19:52:16 -0500

    feat(routes):add estructure general
    (category - location - product - users)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,26 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const brand = require("./brand.js")
-const cart = require("./cart.js")
-const cartItem = require("./cartItem.js")
+const brand = require("./brand.js");
+const cart = require("./cart.js");
+const cartItem = require("./cartItem.js");
+const category = require("./cartItem.js");
+const location = require("./cartItem.js");
+const product = require("./cartItem.js");
+const users = require("./cartItem.js");
 
-const router = Router()
+const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-//probando el pullRequest  
-router.use("/brand", brand)
-router.use("/cart", cart)
-router.use("/cartItem", cartItem)
-
+//probando el pullRequest
+router.use("/brand", brand);
+router.use("/cart", cart);
+router.use("/cartItem", cartItem);
+router.use("/category", category);
+router.use("/location",location);
+router.use("product",product);
+router.use("users",users);
 
 module.exports = router;

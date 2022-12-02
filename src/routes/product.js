f801fdd7c81d88fffadf5236344add991483a041
Diego G <diegoar.guz@hotmail.com>
2022-12-01 20:29:01 -0500

    Creacion de ruta get y get/id

--- src/routes/product.js
+++ src/routes/product.js
@@ -5,8 +5,10 @@ const router = Router();
 const allProductDB = async () => {
     return await Product.findAll({
         include: [
-         { model: Category },
-         { model: Brand}
+         { model: Category,
+           attributes: ['name'] },
+         { model: Brand,
+           attributes: ['name']},
         ]
     });
 }
@@ -16,7 +18,7 @@ router.get('/', async (req, res, next) => {
     const products = await allProductDB()
     try {
         if (name) {
-            let productName = await products.filter(e => e.name.toLowerCase().includes(name.toLowerCase()));
+            let productName = await products.filter(e => e.title.toLowerCase().includes(name.toLowerCase()));
             (productName.length > 0 ? res.json(productName) : res.status(404).json({ message: 'Producto no encontrado'}))
         } else {
             products.length > 0 ? res.json(products) : res.status(404).json({ message: 'No hay productos'})
@@ -24,6 +26,30 @@ router.get('/', async (req, res, next) => {
 } catch (error) {
   next(error);
 }
+});
+
+router.get('/:id', async (req, res) => {
+    let { id } = req.params;
+    const products = await allProductDB()
+    try {
+        products.forEach(el => {
+        if (el.id == id) {
+            res.json({
+                id: el.id,
+                title: el.title,
+                img: el.img || el.img.forEach(i =>{return i}),
+                price: el.price,
+                description: el.description,
+                stock: el.stock,
+                category:el.category.name,
+                brand:el.brand.name
+            })
+          }
+        })
+    } catch (error) {
+        res.status(404).send(error);
+    }
+
 });
 
 module.exports = router;

1b4bad732a573670208c72d40f8f98d4c0a3fdc9
Diego G <diegoar.guz@hotmail.com>
2022-12-12 12:19:49 -0500

    Ruta pasarela de pago

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,24 +1,41 @@
 const { Router } = require("express");
+require('dotenv').config();
 const Stripe = require("stripe")
 const router = Router();
+const {DB_PAYMENT} = process.env;
 
-const stripe = new Stripe("sk_test_51MCUPjIxZNdfrxaOsZ59U0ATCiZvptWltsYPlQ4y5PwhVXLSjds6NlqeII3vVBaxSlk2GXqsTgotPJsGD46BTnw7008OcriKDy");
+const stripe = new Stripe(DB_PAYMENT);
+
 
 router.post('/api/checkout', async (req, res) =>{
-    try {
-        const payment = await stripe.paymentIntents.create({
-            amount:req.body.amount,
-            currency:"USD",
-            description:"compra de prueba",
-            payment_method:req.body.id,
-            confirm:true
-        })
-        console.log(payment)
-        res.send({message:'Pago aceptado'})
-        
-    } catch (error) {
-        res.json({message:error.raw.message})
-    }
+    let product=req.body.products
+    console.log("body",product)
+    let listProduct=[];
+
+    product.forEach(p => {
+        let lineProduct = {
+            price_data:{
+                currency:"usd",
+                product_data:{
+                    name:p.name,
+                    images:["https://www.fqingenieria.com/img/362585/foto.png"]
+                },
+                unit_amount: p.total
+              },
+              quantity:p.quantity
+        }
+        listProduct.push(lineProduct);
+    })
+
+    const session = await stripe.checkout.sessions.create({
+    payment_method_types:["card"],
+    line_items: listProduct,
+    mode: 'payment',
+    success_url: `http://localhost:5173`,
+    cancel_url: `http://localhost:5173/cart`,
+    });
+  
+    res.json({id:session.id})
 })
 
 

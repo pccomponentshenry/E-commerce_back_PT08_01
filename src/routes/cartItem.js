0e73b338ac6849cc4a6addeddf5887aa50eabcc1
carlamadeo <carlamadeo@gmail.com>
2022-12-16 16:13:56 -0300

    Routes logics in the controllers

--- src/routes/cartItem.js
+++ src/routes/cartItem.js
@@ -1,41 +1,9 @@
 const { Router } = require("express");
+const { postCartItem, modifyCartItem } = require("../controllers/cartItem");
 const router = Router();
-const {Product, CartItem} = require ('../db')
+const { Product, CartItem } = require('../db')
 
-router.post("/", async (req, res) => {
-    const { name } = req.body;
-    try {
-        const findProduct = await Product.findOne({
-            where: {
-              title: name,
-            }
-          })
-         // console.log(findProduct)
-      await CartItem.create({
-        productId: findProduct.dataValues.id
-      },
-      )
-      res.send('CartItem created successfully')
-  
-    } catch (error) {
-        
-      res.status(404).json(error.message)
-    }
-  });
-  router.put ( "/:id", async (req, res) => {
-    const { id } = req.params;
-    const { quantity } = req.body;
-  //console.log(id, quantity)
-    try {
-     
-      let forUpdate = await CartItem.findByPk(id);
-      await forUpdate.update({
-        quantity: quantity
-      });
-      res.status(200).send("Product update successfully")
-    } catch (error) {
-      res.status(400).send(error.message)
-    }
-  })
+router.post("/", postCartItem);
+router.put("/:id", modifyCartItem);
 
 module.exports = router;
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,38 +1,8 @@
 const { Router } = require("express");
+const { getUsers, postUser } = require("../controllers/users");
 const router = Router();
-const { Users, Location } = require("../db");
 
-router.get("/", async (req, res) => {
-  try {
-    const allUsers = await Users.findAll({
-       raw: true 
-    });
-    res.status(200).json(allUsers);
-  } catch (error) {
-    res.status(400).json({ error: "No se encontraron Usuarios" });
-  }
-});
-router.post("/", async (req, res) => {
-  const { email, status, isAdmin, location } = req.body;
-  try {
-    /* const findLocation = await Location.findOne({
-      where: {
-        name: location,
-      }
-    }) */
-    await Users.create({
-      username: email,
-      email,
-      status,
-      isAdmin,
-      //LocationId: findLocation.dataValues.id
-    },
-    )
-    res.send('User created successfully')
-
-  } catch (error) {
-    res.status(404).json(error.message)
-  }
-});
+router.get("/", getUsers);
+router.post("/", postUser);
 
 module.exports = router;

b5d9edae41f197bc06fcc2783d96c6e6b7edd3cb
pccomponentshenry <pccomponentshenry@gmail.com>
2022-12-27 21:32:50 -0300

    email registred

--- src/controllers/template.js
+++ src/controllers/template.js
@@ -1,6 +1,5 @@
-const transporter = require('../config/emailer.js')
-const createHttpError = require('http-errors');
-const htmlTemplate = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+const htmlTemplate = 
+`<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" style="width:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;padding:0;Margin:0">
  <head>
   <meta charset="UTF-8">
@@ -291,57 +290,3 @@ a[x-apple-data-detectors] {
   </div>
  </body>
 </html>`
-
-
-
-
-const sendEmailRegister = async (req, res) => {
-    
-    const {email, name}= req.body
-        try {
-            await transporter.sendMail({
-                from: '"PC Components" <pccomponentshenry@gmail.com>', 
-                to: email, 
-                subject: "Email-Registered 📧✔", 
-                html: 
-                htmlTemplate
-              })
-              res.status(201).send('Your email was successfully registered.')
-        } catch (error) {
-            /* const httpError = createHttpError(
-                error.statusCode,
-                `[Error post mailer] - [email - POST]: ${error.message}`,
-              ) */
-              res.status(400).send(error.message)
-            
-        }
-};
-
-
-const sendEmail= async (req, res) => {
-        const { email, name, cart, value }= req.body
-        try {
-            await transporter.sendMail({
-                from: '"PC Components" <pccomponentshenry@gmail.com>', 
-                to: email, 
-                subject: "Purchase Made ✔", 
-                html: 
-                `<h1>Hi! ${name}...</h1>
-                <h2>Thanks for you Purchase</h2>
-                
-                <h3>Purchase detail</h3>
-                
-                <h2>Total: U$D ${value}</h2>` 
-              }); 
-                  res.status(200).send('Email Send')
-                }
-         catch (error) {
-            res.status(400).json({error: error.message})
-        }
-}
-
-
-module.exports = {
-    sendEmailRegister,
-    sendEmail
-}

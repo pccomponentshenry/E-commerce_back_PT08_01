0ddc00530119a7ee3b0d669d8a69a162c653cd99
SantiCalabro <santi.calabro93@gmail.com>
2023-01-01 10:54:44 -0300

    nuevo endpoint de addresses id, para hacer update de uno en específico

--- src/routes/address.js
+++ src/routes/address.js
@@ -3,11 +3,13 @@ const {
   getAddresses,
   postAddress,
   modifyAddress,
+  getAddressById,
 } = require("../controllers/address");
 
 const router = Router();
 
 router.get("/:userId", getAddresses);
+router.get("/:userId/:id", getAddressById);
 router.post("/", postAddress);
 router.put("/", modifyAddress);
 

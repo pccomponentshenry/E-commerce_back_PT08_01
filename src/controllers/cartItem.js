cfd311f6a9a27a9a3ef3abf38c2976df52de4771
carlamadeo <carlamadeo@gmail.com>
2023-01-09 15:10:48 -0300

    Revert "working on favsToDB"
    This reverts commit 1e5b9adf5843bc34796d127c56b80b245f07fb7c.

--- src/controllers/cartItem.js
+++ src/controllers/cartItem.js
@@ -10,8 +10,7 @@ const getCartItem = async (req, res) => {
     JOIN users AS u ON u.id = ci."userId" 
     JOIN categories AS c ON p."categoryId" = c.id 
     JOIN brand AS b ON b.id = p."brandId"
-    WHERE u.email = '${email}'
-    ORDER BY p.id`;
+    WHERE u.email = '${email}'`;
 
   try {
     const products = await conn.query(sqlQuery,
--- src/routes/favorites.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express");
-const { getFavorites, postFavorites, putFavorites } = require("../controllers/favorites");
-const router = Router();
-
-router.get("/:userId", getFavorites);
-router.put("/:userId/:productId", putFavorites);
-router.post("/:userId/:productId", postFavorites);
-
-module.exports = router;
